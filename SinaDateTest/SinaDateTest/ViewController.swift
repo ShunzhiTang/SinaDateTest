57ad13e84a0c51df980c67d43aacb71aa24ba77b
tangShunZhi <1046975221@qq.com>
2015-10-23 16:47:47 -0700

    1、把我们已知的 日期格式转化为字符串表示出来

---
+++ SinaDateTest/SinaDateTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SinaDateTest
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let date:NSDate = NSDate()
+        print(date)
+        demoDate()
+    }
+    //建立一个实例进行测试
+
+    func demoDate() {
+        let string = "Mon Oct 23 12:12:00 +0800 2015"
+        
+        //转换成日期
+        let df = NSDateFormatter()
+        
+        //指定时区
+        df.locale = NSLocale(localeIdentifier: "en")
+        
+        //日期格式化字符串
+        df.dateFormat = "EEEE MMM dd HH:mm:ss zzz yyyy"
+        
+        let date = df.dateFromString(string)
+        print(date)
+    }
+
+
+}
+
